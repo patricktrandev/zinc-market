7168e0d119554adb796b6d47718be9c479ee086e
patricktrandev <patricktrandev@gmail.com>
2025-05-30 02:35:57 +0700

    (project) fix ci build error

--- src/test/java/cloud/tientn/zinc/controller/ProductControllerTest.java
+++ src/test/java/cloud/tientn/zinc/controller/ProductControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
